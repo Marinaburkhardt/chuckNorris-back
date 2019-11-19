e835cbdbeabe892bd534cd54cdf33eff63299b9d
Marina Burkhardt <marina.burkhardt@globant.com>
2019-11-19 20:28:27 -0300

    New changes with sp

--- src/services/timeout/timeout.js
+++ src/services/timeout/timeout.js
@@ -1,23 +1,29 @@
 const mailingChuck = require('../mailingChuck/mailChuck')
-let timeoutID = 300000 //equivalente a 5min
+let timeoutID = 100 //equivalente a 5min = 300000
+let partidaDB = require ('../../daos/daos-db/partida-db-dao')
 
 /**
  * @param {*} primerJugador jugador que realiza la primer jugada dentro del turno
  * @param {*} segundoJugador jugador que realiza la segunda jugada dentro del turno
  * @param {*} idPartida identificador de la partida en juego
  */
-function reivisarTimeoutPartida(ulitmoJugador, proximoJugador, idPartida) {
-    let idJugadaUltimoJugador;
-    let idJugadaProximoJugador;
+function reivisarTimeoutPartida(ulitmoJugador, proximoJugador, idPartida, turnoAntesDeRevision) {
 
-    setTimeout (function(){
-        if (idJugadaProximoJugador == null && idJugadaUltimoJugador <= 10){ //verifica q no haya terminado el juego
-            mailingChuck.envioMailChuck(proximoJugador,'perdisteTimeout')
-            mailingChuck.envioMailChuck(ulitmoJugador,'ganaste')
-            //set partida finalizada
-        } else {
-            clearTimeout(timeoutID)   
-        }
-    }, timeoutID)
+    partidaDB.obtenerDetalles(idPartida).then(result => {
+        let turno = result[0][0].length;
+        let ultimoTurnoActual = turno.NumeroTurno;
+        
+        setTimeout (function(){
+            if (idJugadaProximoJugador == null && turnoAntesDeRevision != ultimoTurnoActual){
+                mailingChuck.envioMailChuck(proximoJugador,'perdisteTimeout')
+                mailingChuck.envioMailChuck(ulitmoJugador,'ganaste')
+                //setear partida finalizada en controlador
+            } else {
+                clearTimeout(timeoutID)   
+            }
+        }, timeoutID)
+    
+    } )
 
+    
 }
